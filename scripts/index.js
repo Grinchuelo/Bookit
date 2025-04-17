e8b1e45d2999321d32657b5a54d98fb4d74d6f12
Grin <santinogreen14@gmail.com>
2025-04-17 15:20:10 -0300

    checkpoint antes de hacer cambios arriesgados en la logica js en signUp deseenme suerte

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,139 @@
-let btnDropdownGenres = document.querySelector('.dropdown-genres');
-let genresList = document.querySelector('.genres-list');
+let timeoutId;
+let charsAllowedUsername = /^[a-zA-Z0-9_]*$/;
 
-btnDropdownGenres.addEventListener('click', () => {
-    genresList.classList.toggle('flex');
+function validate(inputElement) {
+    const inputContainer = inputElement.parentNode;
+    const requirementsContainer = inputElement.nextElementSibling.firstElementChild.nextElementSibling;
+    const svg = inputElement.nextElementSibling.firstElementChild.firstElementChild;
+
+    if (inputElement.classList.contains('usernameInput')) {
+        validateUsername(inputElement, inputContainer, requirementsContainer, svg);
+    }
+    if (inputElement.classList.contains('emailInput')) {
+        validateEmail(inputElement, inputContainer, requirementsContainer, svg);
+    }
+}
+
+function validateUsername(inputElement, inputContainer, requirementsContainer, svg) {
+    // formato de nombre de usuario CORRECTO
+    if (inputElement.value.length == 0 || inputElement.value.length >= 3 && charsAllowedUsername.test(inputElement.value)) {
+        if (inputElement.value.length >= 3 && charsAllowedUsername.test(inputElement.value)) {
+            svg.setAttribute('href', './assets/icons/icons.svg#roundedGreenCheck-icon');
+        }
+        if (inputElement.value.length == 0 && inputElement.classList.contains('noValidateIcon')) {
+            svg.setAttribute('href', '');            
+        } else if (inputElement.value.length == 0) {
+            svg.setAttribute('href', './assets/icons/icons.svg#info-icon');   
+        }
+        successFormatInput(inputElement, inputContainer, requirementsContainer);
+        return
+    }
+    // formato de nombre de usuario INCORRECTO
+    if (inputElement.value.length < 3 || !charsAllowedUsername.test(inputElement.value)) {
+        errorFormatInput(inputContainer, requirementsContainer, svg)
+        return;
+    } 
+}
+
+function validateEmail(inputElement, inputContainer, requirementsContainer, svg) {
+    inputElement.setAttribute('onkeyup', 'validateEmail(this)');
+    const formatEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (formatEmail.test(inputElement.value)) {
+        successFormatInput(inputElement, inputContainer, requirementsContainer)
+    } else {
+        errorFormatInput(inputContainer, requirementsContainer, svg)
+        errorFormatBlurInput(inputElement);
+    }
+}
+// Cuando el usuario rellena el input se hace debounce para entrar a validate o a 
+// displayEye en caso de que el input sea el de password 
+function fillInput(inputElement) {
+    clearTimeout(timeoutId);
+    if (inputElement.getAttribute('id') == "passwordInput") {
+        timeoutId = setTimeout(() => displayEye(inputElement), 100); 
+    }
+
+    timeoutId = setTimeout(() => validate(inputElement), 1200); 
+}
+
+function hideRequirements(inputElement) {
+    const requirementsContainer = inputElement.nextElementSibling.firstElementChild.nextElementSibling;
+    requirementsContainer.style.display = "none";
+
+    if ((inputElement.value.length < 3 && inputElement.value.length > 0) || !charsAllowedUsername.test(inputElement.value)) {
+        errorFormatBlurInput(inputElement);
+    }
+}
+
+// Funcionalidad dinámica para ver o no ver el contenido de la contraseña
+function displayEye(inputElement) {
+    const eyeIcon = document.getElementById('eyeIcon');
+    if (inputElement.value.length >= 0) {
+        eyeIcon.setAttribute('href', './assets/icons/icons.svg#blueClosedEye-icon');
+    } if (inputElement.value.length == 0) {
+        eyeIcon.setAttribute('href', '');
+    }
+}
+
+const eyeIcon = document.getElementById('eyeIcon');
+const passwordInput = document.getElementById('passwordInput');
+eyeIcon.addEventListener('click', () => {
+    passwordInput.focus();
+    eyeIcon.setAttribute('href', './assets/icons/icons.svg#blueEye-icon');
+    passwordInput.setAttribute('type', 'text');
 })
 
-document.querySelector('.dropdown-container').addEventListener('click', () => {
-    document.querySelector('.dropdown-options').classList.toggle('flex');
+// Función para abrir y cerrar requirimentsContainer clicando sobre el svg
+let isDisplayed = false;
+var displayedContainer;
+function displayRequirements(e, svgElement) {
+    e.stopPropagation();
+    // if para incluír también al input de contraseña dado a que su estructura
+    // es distinta a la de los demás 
+    if (svgElement.classList.contains('exceptionForDisplay')) {
+        displayedContainer = svgElement.parentNode.nextElementSibling;
+    } else {
+        displayedContainer = svgElement.nextElementSibling;
+    }
+    displayedContainer.classList.toggle('display');
+
+    if (displayedContainer.classList.contains('display')) {
+        isDisplayed = true;
+    }
+}
+
+function successFormatInput(inputElement, inputContainer, requirementsContainer) {
+    inputContainer.style.borderBottom = "solid 1px #fff";
+    inputContainer.style.borderRadius = "0px";
+    inputContainer.style.backgroundColor = "transparent";
+    inputElement.style.color = "#fff";
+    inputElement.classList.remove("placeholderBlue");
+    inputElement.classList.add("placeholderWhite");
+    requirementsContainer.style.display = "none";
+}
+
+function errorFormatInput(inputContainer, requirementsContainer, svg) {
+    svg.setAttribute('href', './assets/icons/icons.svg#redError-icon');
+    inputContainer.style.borderBottom = "solid 2px #E72323";
+    requirementsContainer.style.display = "block";
+}
+// En caso de que el usuario blurree el input sobre el que escribió
+function errorFormatBlurInput(inputElement) {
+    const inputContainer = inputElement.parentNode;
+
+    inputContainer.style.borderRadius = "1rem";
+    inputContainer.style.backgroundColor = "#FFE2E2";
+    inputElement.style.color = "#3783F5";
+    inputElement.classList.add("placeholderBlue");
+}
+
+// Función para cerrar requirimentsContainer clicando fuera de él
+document.addEventListener('click', (e) => {
+    // Salir en caso de que la requirimentsContainer no esté en display
+    // o en caso de que el usuario haya clicado sobre requirimentsContainer
+    if (!isDisplayed || displayedContainer.contains(e.target)) return;
+    
+    displayedContainer.classList.remove('display');
+    isDisplayed = false;
 })
--- signUp.php
+++ signUp.php
@@ -1,38 +1,149 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/general.css">
-    <link rel="stylesheet" href="./css/logIn-signUp.css">
-    <link rel="stylesheet" href="./css/index.css">
-    <title>Bookit</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/general.css">
+  <link rel="stylesheet" href="./css/index.css">
+  <title>Bookit</title>
 </head>
+
 <body>
-    <div class="main-container">
-        <div class="form-container">
-            <div class="title-container">
-                <h1>Regístrate</h1>
-            </div>
-            <form method="">
-                <div class="email-container input-container">
-                    <label>Correo electrónico</label>
-                    <input type="email" name="email" id="email" maxlength="100">
-                </div>
-                <div class="username-container input-container">
-                    <label>Nombre de usuario</label>
-                    <input type="text" name="username" id="username" maxlength="20">
-                </div>
-                <div class="key-container input-container">
-                    <label>Contraseña</label>
-                    <input type="password" name="key" id="password" maxlength="50">
-                </div>
-                <div class="btn-container">
-                    <button type="submit">REGISTRARME</button>
-                </div>
-            </form>
-            <p>¿Ya tienes una cuenta? <a href="./logIn.php">Inicia sesión</a></p>
-        </div>
+  <div class="main-container">
+    <div class="title-container">
+      <h1>Regístrate</h1>
     </div>
+    <div class="form-container">
+      <form method="">
+        <div class="labelInput-container email-container">
+          <label for="emailInput">Correo electrónico</label>
+          <div class="input-container">
+            <input type="email" name="email" id="emailInput" class="emailInput noValidateIcon" placeholder="Ingrese su correo electrónico" maxlength="50" onkeyup="fillInput(this)" onblur="validateEmail(this)">
+            <div class="svg-container">
+              <svg>
+                <use id="validationSvg" href=""></use>
+              </svg>
+              <div class="requirements-container">
+                <div class="requirements-element">
+                  <span>El correo electrónico no parece válido. Asegúrate de incluir un "@" y un dominio como ".com"</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="labelInput-container username-container">
+          <label for="usernameInput">Nombre de usuario</label>
+          <div class="input-container">
+            <input type="text" name="username" id="usernameInput" class="usernameInput" placeholder="Ingrese un nombre de usuario" maxlength="20" onkeyup="fillInput(this)" onblur="hideRequirements(this)">
+            <div class="svg-container">
+              <svg onclick="displayRequirements(event, this)">
+                <use id="usernameSvg" href="./assets/icons/icons.svg?v=2#info-icon"></use>
+              </svg>
+              <div class="requirements-container">
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>Mínimo 3 caracteres</span>
+                </div>
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>No usar otro caracter que no sea <b>letra</b>, <b>número</b> o <b>_</b> (guión bajo)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="labelInput-container password-container">
+          <label for="passwordInput">Contraseña</label>
+          <div class="input-container">
+            <input type="password" name="password" id="passwordInput" placeholder="Ingrese una contraseña" maxlength="50" onkeyup="fillInput(this)" onblur="hideRequirements(this)">
+            <div class="svg-container">
+              <div class="validationEye-container">
+                <svg id="passwordEye">
+                  <use id="eyeIcon" href=""></use>
+                </svg>
+                <svg class="exceptionForDisplay" onclick="displayRequirements(event, this)">
+                  <use id="validationSvg" href="./assets/icons/icons.svg?v=2#info-icon"></use>
+                </svg>
+              </div>
+              <div class="requirements-container">
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>Mínimo 8 caracteres</span>
+                </div>
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>Mínimo una letra minúscula</span>
+                </div>
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>Mínimo una letra mayúscula</span>
+                </div>
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>Mínimo un número</span>
+                </div>
+                <div class="requirements-element">
+                  <div class="svgRequirement-container">
+                    <svg>
+                      <use href=""></use>
+                    </svg>
+                  </div>
+                  <span>Mínimo un carácter especial <br>(!, @, #, $, %, -, &, *, etc.)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="labelInput-container validatePassword-container">
+          <label for="validatePasswordInput">Validar contraseña</label>
+          <div class="input-container">
+            <input type="password" name="validatePasswordInput" class="noValidateIcon" id="validatePasswordInput" placeholder="Ingrese de nuevo la contraseña" maxlength="50" onkeyup="fillInput(this)" onblur="hideRequirements(this)">
+            <div class="svg-container">
+              <svg>
+                <use id="validationSvg" href=""></use>
+              </svg>
+              <div class="requirements-container">
+                <div class="requirements-element">
+                  <span>Debe coincidir con la contraseña ingresada</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="submitBtn-container">
+          <button type="submit">REGISTRARME</button>
+          <p>¿Ya tienes una cuenta? <a href="./logIn.php">Inicia sesión</a></p>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <script src="./scripts/index.js"></script>
 </body>
+
 </html>
